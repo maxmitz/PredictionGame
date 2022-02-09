02461568d4913d003a127f01717ea2a2c69c5284
maxmitz <m-zimmermann1@gmx.net>
2022-02-09 23:43:26 +0100

    Deleted unnecassary code

--- lib/components/untere_leiste.dart
+++ lib/components/untere_leiste.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Tippen/components/body.dart';
 
-import 'package:flutter_auth/Screens/Uebersicht/components/body.dart';
 import 'package:flutter_auth/components/navigation_bar.dart';
 
 class UntereLeiste extends StatelessWidget {

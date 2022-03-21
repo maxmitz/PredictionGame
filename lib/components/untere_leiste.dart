7d2a11797b7193d7efcaaa72448cb76d27fad414
maxmitz <m-zimmermann1@gmx.net>
2022-03-21 12:23:32 +0100

    Fixed StreamPovider Bug

--- lib/components/untere_leiste.dart
+++ lib/components/untere_leiste.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Tippen/components/body.dart';
+import 'package:flutter_auth/Services/database.dart';
 
 import 'package:flutter_auth/components/navigation_bar.dart';
+import 'package:flutter_auth/models/user.dart';
+import 'package:provider/provider.dart';
 
 class UntereLeiste extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Body(), bottomNavigationBar: NavigationBar());
+    final user = Provider.of<TheUser>(context);
+
+    return StreamProvider<UserData>.value(
+        value: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          return Scaffold(body: Body(), bottomNavigationBar: NavigationBar());
+        });
   }
 }

de3ff3cf95b8295d8def13227963ed13ce1bfc36
maxmitz <m-zimmermann1@gmx.net>
2022-06-22 19:21:01 +0200

    Added new colors

--- lib/shared/tpnColors.dart
+++ lib/shared/tpnColors.dart
@@ -4,4 +4,6 @@ class TPNColors {
   TPNColors._();
   static const Color darkGreen = Color(0xFF046A38);
   static const Color lightGreen = Color(0xFF86BC25);
+  static const Color lightGrey = Color(0xFFD0D0CE);
+  static const Color superLightGreen = Color(0xFFDDEFE8);
 }

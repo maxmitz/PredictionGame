b226ece22b528fd0b181a631e3874a63166cb792
maxmitz <m-zimmermann1@gmx.net>
2022-06-24 17:01:18 +0200

    Deleted unnecessary class

--- lib/Screens/Tippen/tippen_screen.dart
+++ lib/Screens/Tippen/tippen_screen.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Tippen/components/body.dart';
+import 'package:flutter_auth/Screens/Tippen/components/gameDayWidget.dart';
+import 'package:flutter_auth/Screens/onboarding/helpOnboarding.dart';
 import 'package:flutter_auth/Services/database.dart';
+import 'package:flutter_auth/models/game.dart';
 import 'package:flutter_auth/models/user.dart';
+import 'package:flutter_auth/shared/constants.dart';
 import 'package:flutter_auth/shared/loading.dart';
 import 'package:provider/provider.dart';
 
 class TippenScreen extends StatelessWidget {
+  const TippenScreen({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<TheUser?>(builder: (_, user, __) {
-      if (user != null) {
-        return StreamProvider<UserData?>.value(
-          value: DatabaseService(uid: user.uid).userData,
-          builder: (context, snapshot) {
-            return Scaffold(
-              body: Body(),
-            );
-          },
-          initialData: null,
-        );
+    return Consumer<UserData?>(builder: (_, userdata, __) {
+      if (userdata != null) {
+        return StreamProvider<List<List<Game>>?>.value(
+            value: DatabaseService(ligen: userdata.ligen).gameday,
+            initialData: null,
+            child: MaterialApp(
+                home: Scaffold(
+                    resizeToAvoidBottomInset: false,
+                    appBar: AppBar(
+                        backgroundColor: kPrimaryColor,
+                        title: Text(
+                          'Tippen',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        actions: <Widget>[
+                          TextButton.icon(
+                              style:
+                                  TextButton.styleFrom(primary: Colors.black),
+                              icon: Icon(Icons.help_outline),
+                              label: Text('Hilfe'),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => HelpOnboarding()));
+                              }),
+                        ]),
+                    body: SizedBox.expand(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/main_top.png'),
+                                fit: BoxFit.cover)),
+                        child: GameDayWidget(),
+                      ),
+                    ))));
       } else {
         return Loading();
       }

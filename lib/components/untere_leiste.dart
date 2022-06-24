b2cdac848712a27647acaf182b603837c1b9efff
maxmitz <m-zimmermann1@gmx.net>
2022-06-24 18:32:03 +0200

    Chenged navigation because tippen body was deleted

--- lib/components/untere_leiste.dart
+++ lib/components/untere_leiste.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Tippen/components/body.dart';
 import 'package:flutter_auth/Services/database.dart';
 
 import 'package:flutter_auth/components/navigation_bar.dart';
 import 'package:flutter_auth/models/user.dart';
+import 'package:flutter_auth/screens/tippen/tippen_screen.dart';
 import 'package:flutter_auth/shared/loading.dart';
 import 'package:provider/provider.dart';
 
@@ -16,7 +16,8 @@ class UntereLeiste extends StatelessWidget {
           value: DatabaseService(uid: user.uid).userData,
           builder: (context, snapshot) {
             return Scaffold(
-                body: Body(), bottomNavigationBar: CustomNavigationBar());
+                body: TippenScreen(),
+                bottomNavigationBar: CustomNavigationBar());
           },
           initialData: null,
         );
